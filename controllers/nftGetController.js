09e87d1c98532983bbe58ca93210b7c0b489ac93
Krieger28 <ivankoch28@gmail.com>
2022-07-06 22:08:17 -0300

    solucionando error de branch

--- controllers/nftGetController.js
+++ controllers/nftGetController.js
@@ -1,6 +1,7 @@
 
 
  const getAllNft = async (req, res) => {
+res.status(200).send()
  res.send(console.log("hola"))
 }
 
@@ -12,3 +13,21 @@ module.exports = {
     getAllNft,
     getIdNft
 }
+
+// const options = { q: "Pancake", chain: "bsc", filter: "name" };
+// const NFTs = await Moralis.Web3API.token.searchNFTs(options);
+
+
+// async function query() {
+//     try {
+//         const options = { q: "Pancake", chain: "bsc", filter: "name" };
+//         const NFTs2 = await Moralis.Web3API.token.searchNFTs(options);
+//         console.log('acaaaaaaa2', NFTs2);
+//     } catch (error) {
+//         console.log(error) 
+//     }
+// }
+
+//npm i moralis 
+
+// moralis.start({serverUrl, appId});
