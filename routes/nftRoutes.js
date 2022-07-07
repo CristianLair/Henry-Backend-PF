ee2e23a2dcf79cf021e64cc057adddc61fb70b62
Krieger28 <ivankoch28@gmail.com>
2022-07-07 19:47:31 -0300

    routes by name

--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -1,36 +1,35 @@
-const { default: axios } = require('axios');
-const express = require('express');
+const { default: axios } = require("axios");
+const express = require("express");
 
 const router = express.Router();
 
-const Moralis = require('moralis/node');
-const { getAllNft } = require('../controllers/nftGetController');
-const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server"
-const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi"
-const masterKey = 'bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB'
-Moralis.start({ serverUrl, appId , masterKey});
+const Moralis = require("moralis/node");
+const { getAllNft, getNameNft } = require("../controllers/nftGetController");
+const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server";
+const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi";
+const masterKey = "bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB";
+Moralis.start({ serverUrl, appId, masterKey });
 
-const createNft = require('../controllers/nftPostController')
+const createNft = require("../controllers/nftPostController");
 
 // GET all nfts
-router.get('/nfts/:name', getAllNft)
-  
+router.get("/nfts/:name", getAllNft);
+router.get("/nfts/", getNameNft);
 
-   //
-  // GET a single nft by token id
-  router.get('/nft/:id', async (req, res) => {
-    const {id} = req.params
-    const options = { q: id , chain: "bsc", filter: "name" };
-    const NFTs = await Moralis.Web3API.token.searchNFTs(options);
-    res.status(200).json(NFTs)
-  })
-  
-    
-  // POST a new nft
-  router.post('/nft', createNft)
-  
-   //testingtestingtestginggg
+//
+// GET a single nft by token id
+router.get("/nft/:id", async (req, res) => {
+  const { id } = req.params;
+  const options = { q: id, chain: "bsc", filter: "name" };
+  const NFTs = await Moralis.Web3API.token.searchNFTs(options);
+  res.status(200).json(NFTs);
+});
 
- //testingtestingtestginggg
+// POST a new nft
+router.post("/nft", createNft);
+
+//testingtestingtestginggg
+
+//testingtestingtestginggg
 
 module.exports = router;
