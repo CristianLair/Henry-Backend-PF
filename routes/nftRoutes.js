912da0c7cb6202ffce22dffa363dbf8b6d24c27e
Matiasballesta <75946070+Matiasballesta@users.noreply.github.com>
2022-07-07 13:14:16 -0300

    Made some changes fellas

--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -8,18 +8,21 @@ const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi"
 const masterKey = 'bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB'
 Moralis.start({ serverUrl, appId , masterKey});
 
-
 const createNft = require('../controllers/nftPostController')
 
 // GET all nfts
 router.get('/nfts/:type', async (req, res) => {
   const {type} = req.params
-    
+
     try {
         const options = { q: type , chain: "bsc", filter: "name" };
         const NFTs = await Moralis.Web3API.token.searchNFTs(options);
-        
-        res.send(NFTs)
+        const finalGet = NFTs.result.map(el => el.metadata.toString().replace(/ /g, ""))
+        const final = finalGet.map(el => el.replace(/(\r\n|\n|\r)/gm,""))
+        const finaldata = final.map(el => el.replace(/[\/\\]+/g,''))
+       
+        res.send(finaldata)
+
         
     } catch (error) {
         console.log(error)
