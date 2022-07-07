4f892f9a4f6b4532daba957c537428f445cb623a
Krieger28 <ivankoch28@gmail.com>
2022-07-07 17:31:37 -0300

    getAllNft endpoint

--- controllers/nftGetController.js
+++ controllers/nftGetController.js
@@ -1,19 +1,36 @@
+// const { all } = require("../routes/nftRoutes");
+const Moralis = require('moralis/node');
+const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server"
+const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi"
+const masterKey = 'bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB'
+Moralis.start({ serverUrl, appId , masterKey});
+
 
  const getAllNft = async (req, res) => {
     const {type} = req.params
-    const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server"
-    const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi"
-    Moralis.start({ serverUrl, appId });
-    try {
-        const options = { q: type , chain: "bsc", filter: "name" };
-        const NFTs = await Moralis.Web3API.token.searchNFTs(options);
-        res.send(NFTs)
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
+      
+      try {
+          const options = { q: "cat" , chain: "bsc", filter: "name" };
+          const NFTs = await Moralis.Web3API.token.searchNFTs(options);
+          const NftsResults = NFTs.result.map((nft)=> Number(nft.token_id));
+          
+          const NftData = NFTs.result.map((nft) => JSON.parse(nft.metadata));
+          for (let i = 0; i < NftData.length; i++) {
+            Object.assign(NftData[i], {token_id: NftsResults[i]});
+          }
+          res.status(200).json(await NftData)
+          
+      } catch (error) {
+          console.log(error)
+      }
+  }
 //
+
+const getNameNft = async (req, res) => {
+    res.send(console.log("hola"))
+}
+
+
  const getIdNft = async (req, res) => {
     res.send(console.log("hola"))
 }
--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -1,8 +1,10 @@
+const { default: axios } = require('axios');
 const express = require('express');
 
 const router = express.Router();
 
 const Moralis = require('moralis/node');
+const { getAllNft } = require('../controllers/nftGetController');
 const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server"
 const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi"
 const masterKey = 'bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB'
@@ -12,23 +14,15 @@ Moralis.start({ serverUrl, appId , masterKey});
 const createNft = require('../controllers/nftPostController')
 
 // GET all nfts
-router.get('/nfts/', async (req, res) => {
-  const {type} = req.params
-    
-    try {
-        const options = { q: "cat" , chain: "bsc", filter: "name" };
-        const NFTs = await Moralis.Web3API.token.searchNFTs(options);
-        const filterNfts = NFTs.result;
-        await filterNfts.map((data)=> data.metadata.metadata)
-        res.status(200).json(filterNfts)
-        
-    } catch (error) {
-        console.log(error)
-    }
-} )
+router.get('/nfts/', getAllNft )
    //
-  // GET a single nft
-  router.get('/nft/:id')
+  // GET a single nft by token id
+  router.get('/nft/:id', async (req, res) => {
+    const {id} = req.params
+    const options = { q: id , chain: "bsc", filter: "name" };
+    const NFTs = await Moralis.Web3API.token.searchNFTs(options);
+    res.status(200).json(NFTs)
+  })
   
   // POST a new nft
   router.post('/nft', createNft)
