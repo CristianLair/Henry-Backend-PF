c3924e323298e7cd45aa1be0cf7080ab7c7c623d
Flo Aguirre <aguirreflomik@gmail.com>
2022-07-07 16:02:24 -0300

    ruta post creacion de nft

--- controllers/nftPostController.js
+++ controllers/nftPostController.js
@@ -1,5 +1,15 @@
+ const Nft = require('../models/Nft')
+ 
  const createNft = async (req, res) => {
-    res.send(console.log("hola"))
+    const {name, description, image } = req.body;
+
+    try {
+        const nft = await Nft.create({name, description, image})
+        res.status(200).json(nft)
+    } catch (error) {
+        res.status(400).json({error : error.message})
+    }
+
 }
 
 module.exports = createNft
