1de5ef92d322ed2b3add880d73ec0994ac83b2f3
Flo Aguirre <aguirreflomik@gmail.com>
2022-07-10 18:31:57 -0300

    postController

--- controllers/nftPostController.js
+++ controllers/nftPostController.js
@@ -1,21 +1,14 @@
 const Nft = require('../models/Nft')
 const Moralis = require("moralis/node");
-const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server";
-const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi";
-const masterKey = "bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB";
-Moralis.start({ serverUrl, appId, masterKey });
- 
+
  const createNft = async (req, res) => {
     
 
     try {
         const {name, description, image } = req.body;
-        const data = image.files[0];
-        const imageFile = new Moralis.File(data.name, data)
-        await imageFile.saveIPFS()
-        const imageURI = imageFile.ipfs();
         
-        const nft = await Nft.create({name, description, imageURI})
+        
+        const nft = await Nft.create({name, description, image })
         res.status(200).json(nft)
     } catch (error) {
         res.status(400).json({error : error.message})
