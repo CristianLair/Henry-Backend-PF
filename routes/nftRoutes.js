7b4f853fdc0b5ae5dd93a5a4eb74e2f1e2eb9939
Flo Aguirre <aguirreflomik@gmail.com>
2022-07-11 14:52:51 -0300

    getNftTests

--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 const Moralis = require("moralis/node");
 const { getAllNft, getNameNft, getIdNft } = require("../controllers/nftGetController");
-const {getNftsComplete} = require('../controllers/pruebaController')
+const {getNftsComplete, getNftId} = require('../controllers/pruebaController')
 const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server";
 const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi";
 const masterKey = "bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB";
@@ -17,7 +17,8 @@ const createNft = require("../controllers/nftPostController");
 router.get("/nfts/:name", getAllNft);
 router.get("/nfts/", getNameNft);
 router.get("/nft/:id", getIdNft);
-router.get('/tests', getNftsComplete )
+router.get('/tests', getNftsComplete );
+router.get('/tests/nftid', getNftId);
 router.post("/nft", createNft);
 
 
