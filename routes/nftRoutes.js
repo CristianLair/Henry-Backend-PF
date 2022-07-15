4bc24ba5361304ba624eb25f09e1077724ad3a62
Flo Aguirre <aguirreflomik@gmail.com>
2022-07-15 15:45:24 -0300

    rutas colecciones

--- routes/nftRoutes.js
+++ routes/nftRoutes.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 const Moralis = require("moralis/node");
 const { getAllNft, getNameNft, getIdNft } = require("../controllers/nftGetController");
-const {getNftsComplete, getNftId} = require('../controllers/pruebaController')
+const {getNftsComplete, getNftId, getOneCollection, getNFTPrice} = require('../controllers/pruebaController')
+const {getNFTCollection} = require('../controllers/collectionsController')
 const serverUrl = "https://hzgmh0bhktiz.usemoralis.com:2053/server";
 const appId = "TvlbElMKEQ3ozadXOqUAthnvVYSIKgNIIrllWHBi";
 const masterKey = "bJ7z3DlllOjtYp1fRdf4ITSOXh6ewwvZEyR1nOQB";
@@ -19,6 +20,9 @@ router.get("/nfts/", getNameNft);
 router.get("/nft/id", getIdNft);
 router.get('/tests', getNftsComplete );
 router.get('/tests/nftid', getNftId);
+router.get('/nftonecolleccion', getOneCollection);
+router.get('/tests/price', getNFTPrice)
+router.get('/nftcollection', getNFTCollection )
 router.post("/nft", createNft);
 
 
