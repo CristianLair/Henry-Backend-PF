973410b939a388b5017f98e59b4c28806dbd2187
crisitan lair <crisitanlair@gmail.com>
2022-08-17 17:40:05 -0300

    prueba

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const authRouter = require("./routes/googleRoutes");
 
 const express = require("express");
 const mongoose = require("mongoose");
-const {nftRoutes} = require('./controllers/nftGetController')
+const nftRoutes = require("./routes/nftRoutes");
 const user = require("./controllers/usuarioController");
 const Usuario = require("./models/user");
 const authUser = require("./controllers/authController");
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.get("/api", nftRoutes);
+app.use("/api", nftRoutes);
 app.use("/api/registro", user, checkRolesExisted);
 app.use("/api/login", authUser);
 app.put("/:email/updatePassword", (req, res, next) => {
